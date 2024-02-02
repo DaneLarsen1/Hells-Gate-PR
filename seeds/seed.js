1b3f454cd251d99ce82da3b66a9dd719cd13b44a
Tony Pinski <147113871+Verred01@users.noreply.github.com>
2024-02-01 19:02:50 -0700

    added seeds folder

---
+++ seeds/seed.js
@@ -0,0 +1,17 @@
+const sequelize = require('');
+const { User } = require('');
+
+const userData = require('');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
