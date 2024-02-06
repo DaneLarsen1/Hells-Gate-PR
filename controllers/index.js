57d161df65375332e3af677511eb2b1bcf8ebcbc
RMP <ruthiepina@gmail.com>
2024-02-05 21:48:39 -0700

    Fixed paths in auth.js and conection.js, added dummy seed data for Lifts.

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,13 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const apiRoutes = require('./api');
-const frontEndRoutes = require('./frontEndRoutes');
+const apiRoutes = require("./api");
+const frontEndRoutes = require("./frontEndRoutes.js"); //* added .js to file path
 
-router.use('/', frontEndRoutes);
-router.use('/api', apiRoutes);
+router.use("/", frontEndRoutes);
+router.use("/api", apiRoutes);
+
+// router.use((req, res) => {
+//    res.status(404).end();
+// });
 
 module.exports = router;
